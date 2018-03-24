f23913123f88c734590624c2156a034099f72f89
cmccloney <cmccloney@hotmail.com>
2018-03-24 11:56:08 -0600

    working?

---
+++ src/main/java/JavaApplication38/src/csci305/javalab/Element.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci305.javalab;
+
+/**
+ *
+ * @author Conner McCloney
+ */
+public class Element {
+    public static void main(String args[]){
+        System.out.println("H");
+    }
+}
